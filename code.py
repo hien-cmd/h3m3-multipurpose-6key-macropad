3ea1983ba94c387b72394b36edbe19e3100f3ec1
hien-cmd <hien040661@stu.vinschool.edu.vn>
2026-09-01 20:10:40 +0700

    Updated code.py so that keybinds and settings can be modified through a config.h file

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
-#replace the codee.py file in the "CIRCUITPY" directory with this!
-#make sure to install "adafruit_hid" and "kmk" libraries!
+#replace the code.py file in the "CIRCUITPY" directory with this!
+#make sure to install adafruit_hid and kmk libraries
 import time
 import board
 import analogio
+import json
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
@@ -10,6 +11,30 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners.keypad import KeysScanner
 from kmk.modules import Module
+
+#this helper function map string names from json config file to kmk/hidkeycodes
+def parse_key(key_str):
+    return getattr(KC, key_str.upper(), KC.NO), getattr(Keycode, key_str.upper(), Keycode.A)
+
+#load configs
+try:
+    with open("/config.json", "r") as f:
+        config = json.load(f)
+except Exception as e:
+    print(f"[ERROR] failed to load config.json: {e}")
+    config = {
+        "mechanical_keys": ["RIGHT", "DOWN", "LEFT"],
+        "hall_effect": {
+            "GP27": {"key": "A", "actuation": 0.00},
+            "GP28": {"key": "B", "actuation": 0.00},
+            "GP29": {"key": "C", "actuation": 0.00}
+        },
+        "other_settings": {
+            "reject_val": 1.5,
+            "debounce": 0.1,
+            "confirm_samples": 2
+        }
+    }
 
 #this class reads the hall effect sensors and injects the key presses into KMK's hid report alongside with the mechanical key matrux
 class hallEffectModule(Module):
@@ -92,36 +117,34 @@ class hallEffectModule(Module):
 keyboard = KMKKeyboard()
 
 #mechanical switch setup
+m_keys = [getattr(KC, k.upper(), KC.NO) for k in config["mechanical_keys"]]
 keyboard.matrix = KeysScanner(
     pins=[board.GP0, board.GP1, board.GP2], #set your mechanical pins here, i do 0, 1 and 2
     value_when_pressed=False, 
 )
-keyboard.keymap = [
-    [KC.A, KC.B, KC.C] #customize the output for mechanical switches here
-]
+keyboard.keymap = [m_keys]
 
 #hall effect switches setup
-hall_module = hallEffectModule(
-    sensor_pins = { #set your hall effect sensor pins here, i do 27, 28, 29
-        "GP27": board.GP27,
-        "GP28": board.GP28,
-        "GP29": board.GP29,
-    },
-    keymap = { #customize the output for hall effect switches here
-        "GP27": Keycode.A,
-        "GP28": Keycode.Z,
-        "GP29": Keycode.X,
-    },
+sensor_pins = {
+    "GP27": board.GP27, 
+    "GP28": board.GP28, 
+    "GP29": board.GP29
+}
+he_keymap = {}
+actu_V = {}
 
-    #hall effect sensor calibration details(see README for more info on this)
-    actu_V={ 
-        "GP27": 2.25,
-        "GP28": 2.25,
-        "GP29": 2.35,
-    },
-    reject_val = 1.5,
-    debounce = 0.1,
-    confirm_samples=2,
+for name, data in config["hall_effect"].items():
+    _, hid_key = parse_key(data["key"])
+    he_keymap[name] = hid_key
+    actu_V[name] = data["actuation"]
+
+hall_module = hallEffectModule(
+    sensor_pins=sensor_pins,
+    keymap=he_keymap,
+    actu_V=actu_V,
+    reject_val=config["other_settings"]["reject_val"],
+    debounce=config["other_settings"]["debounce"],
+    confirm_samples=config["other_settings"]["confirm_samples"],
 )
 keyboard.modules.append(hall_module)
 
